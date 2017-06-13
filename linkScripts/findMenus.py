13620a58715783017f89b7e5ccf4f02410351e02
gwiedeman <gwiedeman@albany.edu>
2017-06-13 06:04:51 -0400

    script now reads all warcs

--- linkScripts/findMenus.py
+++ linkScripts/findMenus.py
@@ -8,41 +8,45 @@ hrefs = []
 
 controlList = ["b", "small", "span", "h1,", "h2", "h3", "h4", "h5", "i"]
 
-rawFile = "/home/ubuntu/results_2017_London/part-00000"
+resultsDir = "/home/ubuntu"
+for folder in os.path.listdir(resultsDir):
+	if "results_2017_London" in folder:
+		fullPath = os.path.join(resultsDir, folder, "part-00000")
 
-inputData = open(rawFile, "rb")
-inputText = inputData.read()
 
-startFix = "<html>" + inputText.split("<html>", 1)[1]
-endFix = startFix.rsplit("</html>", 1)[0] + "</html>"
+		inputData = open(fullPath, "rb")
+		inputText = inputData.read()
 
-pageCount = 0
-for thing in endFix.split("<html>"):
-	page = "<html>" + thing
-	soup = BeautifulSoup(page)
-	for link in soup("a"):
-		linkCount += 1
-		href = link.get('href', None)
-		hrefs.append(href)
-		parentTag = link.parent.name
-		
-		parentMatch = False
-		for item in parents:
-			if item["parents"] == parentTag:
-				parentMatch = True
-				item["count"] = item["count"]  + 1
-		if parentMatch == False:
-			parents.append({"count": 1, "parents": parentTag})
-		
-	print "Total Links: " + str(linkCount)
-	pageCount += 1
-	print pageCount
-	print parents
+		startFix = "<html>" + inputText.split("<html>", 1)[1]
+		endFix = startFix.rsplit("</html>", 1)[0] + "</html>"
+
+		pageCount = 0
+		for thing in endFix.split("<html>"):
+			page = "<html>" + thing
+			soup = BeautifulSoup(page)
+			for link in soup("a"):
+				linkCount += 1
+				href = link.get('href', None)
+				hrefs.append(href)
+				parentTag = link.parent.name
+				
+				parentMatch = False
+				for item in parents:
+					if item["parents"] == parentTag:
+						parentMatch = True
+						item["count"] = item["count"]  + 1
+				if parentMatch == False:
+					parents.append({"count": 1, "parents": parentTag})
+				
+			print "Link Count: " + str(linkCount)
+			pageCount += 1
+			print "Page Count: " + str(pageCount)
+			print parents
 
 
 inputData.close()
 
-outPath = "/home/ubuntu/archivesUnleashed2017/output/findMenusOut.json"
+outPath = "/home/ubuntu/archivesUnleashed2017/output/allWarcs.json"
 outputFile = open(outPath, "w")
 json.dump(parents, outputFile)
 outputFile.close()
